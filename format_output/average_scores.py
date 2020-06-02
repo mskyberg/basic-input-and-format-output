7086b866036284febfaa6f0cd845a153b02dd7c0
mskyberg <mskyberg@gmail.com>
2020-06-02 14:57:21 -0500

    updated docstrings and implemented average calculation

--- format_output/average_scores.py
+++ format_output/average_scores.py
@@ -3,9 +3,13 @@ Program: test_average_scores.py
 Author: Michael Skyberg, mskyberg@dmacc.edu
 Last date modified: June 2020
 
-Purpose:
+Purpose: calculate the average test scores
 """
 
 
 def average():
-    pass
+    score_one = input('Please Enter Score 1:')
+    score_two = input('Please Enter Score 2:')
+    score_three = input('Please Enter Score 3:')
+
+    return (float(score_one) + float(score_two) + float(score_three)) / 3
--- test/test_average_scores.py
+++ test/test_average_scores.py
@@ -3,7 +3,7 @@ Program: test_average_scores.py
 Author: Michael Skyberg, mskyberg@dmacc.edu
 Last date modified: June 2020
 
-Purpose:
+Purpose: Test average calculation with mock input
 """
 
 import unittest
